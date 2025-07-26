b97df19c496227d36525018fb3af5fe247b80578
masood <abdurrafeym@gmail.com>
2025-07-26 16:45:05 +0100

    chore: add @vercel/analytics package and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
 import { CartProvider } from '@/features/cart'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Ramé Sivano",
@@ -20,6 +21,7 @@ export default function RootLayout({
           {children}
         </CartProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
